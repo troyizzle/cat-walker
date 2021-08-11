ba008dec14d6be690c50944efe1b04fbb5858dd3
Troy Underwood <troyizzle@gmail.com>
2021-08-11 12:49:03 -0400

    Modify migration and run it

--- db/migrate/20210811164751_create_appointments.rb
+++ db/migrate/20210811164751_create_appointments.rb
@@ -4,7 +4,7 @@ class CreateAppointments < ActiveRecord::Migration[6.1]
       t.references :walker, null: false, foreign_key: true
       t.references :book, null: false, foreign_key: true
       t.references :cat, null: false, foreign_key: true
-      t.integer :status
+      t.integer :status, default: 0
 
       t.timestamps
     end
